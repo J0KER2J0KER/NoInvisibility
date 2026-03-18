6dd47773c2eb1020ba0e906853ee07ef27f8100f
J0KER2J0KER <furs-weekday1q@icloud.com>
2026-03-18 10:52:43 +0100

    - uploaded missing files

--- src/client/java/com/j0ker2j0ker/noinvis/client/NoinvisClient.java
+++ src/client/java/com/j0ker2j0ker/noinvis/client/NoinvisClient.java
@@ -1,10 +1,35 @@
 package com.j0ker2j0ker.noinvis.client;
 
 import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.fabric.api.client.command.v2.ClientCommandRegistrationCallback;
+import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientLifecycleEvents;
+import net.minecraft.network.chat.Component;
+
+import static net.fabricmc.fabric.api.client.command.v2.ClientCommandManager.literal;
 
 public class NoinvisClient implements ClientModInitializer {
 
+    public static InvisConfig CONFIG;
+
     @Override
     public void onInitializeClient() {
+        CONFIG = InvisConfig.load();
+
+        ClientCommandRegistrationCallback.EVENT.register((dispatcher, registryAccess) -> {
+            dispatcher.register(
+                    literal("invis").executes(context -> {
+                        CONFIG.noInvis = !CONFIG.noInvis;
+
+                        context.getSource().sendFeedback(
+                                Component.literal("§oEverything is " + (CONFIG.noInvis ? "now visible." : "back to normal."))
+                        );
+                        return 1;
+                    })
+            );
+        });
+
+        ClientLifecycleEvents.CLIENT_STOPPING.register(client -> {
+            CONFIG.save();
+        });
     }
 }
--- src/client/java/com/j0ker2j0ker/noinvis/client/mixins/EntityMixin.java
+++ src/client/java/com/j0ker2j0ker/noinvis/client/mixins/EntityMixin.java
@@ -1,4 +1,18 @@
 package com.j0ker2j0ker.noinvis.client.mixins;
 
+import com.j0ker2j0ker.noinvis.client.NoinvisClient;
+import net.minecraft.world.entity.Entity;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+@Mixin(Entity.class)
 public class EntityMixin {
+
+    @Inject(method = "isInvisibleTo", at = @At("HEAD"), cancellable = true)
+    public void isInvisibleTo(CallbackInfoReturnable<Boolean> cir) {
+        if (NoinvisClient.CONFIG.noInvis)cir.setReturnValue(false);
+    }
+
 }
